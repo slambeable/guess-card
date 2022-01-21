533f5085b5de8b9ee98ebe7520f2cda977f7b774
slambeable <41022916+slambeable@users.noreply.github.com>
2022-01-21 13:45:02 +0300

    Initial Commit

---
+++ GuessСard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GuessСard
+//
+//  Created by Андрей Евдокимов on 21.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
